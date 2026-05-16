28ffdb49df4a2606d596d341fe628b576a732668
Utkarsh Tripathi <utripathi063@gmail.com>
2026-05-16 20:50:35 +0530

    Create missing axios API configuration file to fix build error

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,21 @@
 import axios from "axios";
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api" });
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
 api.interceptors.request.use((config) => {
-  const user = JSON.parse(localStorage.getItem("user"));
-  if (user?.token) config.headers.Authorization = `Bearer ${user.token}`;
+  const user = localStorage.getItem("user");
+  if (user) {
+    const { token } = JSON.parse(user);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
   return config;
 });
-api.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    if (err.response?.status === 401) { localStorage.removeItem("user"); window.location.href = "/login"; }
-    return Promise.reject(err);
-  }
-);
+
 export default api;
